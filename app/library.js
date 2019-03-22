6ab56d56fadbad7fb032ec00e3ee4521c423cd02
David Underwood <dunderwood13@hotmail.co.uk>
2019-03-22 08:41:58 +0000

    so far needs fixing

--- app/library.js
+++ app/library.js
@@ -3,7 +3,15 @@ class Library {
     this.books = books;
   }
 
-  add() {}
+  add(name, author, ISBN) {
+    const book1 = {name:name, author:author, ISBN:ISBN, available:true};
+    this.books.push(book1);
+  }
+  withdraw(library,ISBN){
+
+    library.filter(bok => bok.ISBN != ISBN);
+  }
 }
+
 
 module.exports = Library;
--- test/library.test.js
+++ test/library.test.js
@@ -2,15 +2,37 @@ const Library = require("../app/library");
 
 test("Check that a library can be instantiated with some books", () => {
   const library = new Library([
-    // Add some books here
+    {name:"Don't Make Me Think", author:"Steve Krug", ISBN: 123, available: true},
+    {name:"Site Reliability Engineering", 
+    author: "Niall Richard Murphy", ISBN:456, available: true},
+    {name:"Fantastic Mr Fox", author:"Roald Dahl", ISBN:789, available:true}
   ]);
   expect(library.books).toEqual([
-    // How should the books look?
+    {name:"Don't Make Me Think", author:"Steve Krug", ISBN: 123, available: true},
+    {name:"Site Reliability Engineering", 
+    author: "Niall Richard Murphy", ISBN:456, available: true},
+    {name:"Fantastic Mr Fox", author:"Roald Dahl", ISBN:789, available:true}
+  ]);
+});
+
+test("Check that a book has been removed", () => {
+  const library = new Library([
+    {name:"Don't Make Me Think", author:"Steve Krug", ISBN: 123, available: true},
+    {name:"Site Reliability Engineering", 
+    author: "Niall Richard Murphy", ISBN:456, available: true},
+    {name:"Fantastic Mr Fox", author:"Roald Dahl", ISBN:789, available:true}
+  ]);
+  library.withdraw(456);
+  expect(library.books).toEqual([
+    {name:"Don't Make Me Think", author:"Steve Krug", ISBN: 123, available: true},
+    {name:"Fantastic Mr Fox", author:"Roald Dahl", ISBN:789, available:true}
   ]);
 });
 
 test("Check that a book can be added to the library", () => {
   const library = new Library([]);
-  library.add();
-  expect(library.books).toEqual([]);
+  library.add("Growth Mindset", "Carol Dweck", 992);
+  expect(library.books).toEqual([
+    {name:"Growth Mindset", author:"Carol Dweck", ISBN:992, available:true}
+  ]);
 });
